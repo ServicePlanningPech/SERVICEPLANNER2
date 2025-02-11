44aa60f62560036227447f1ced8f734744e22118
Alan Reeves <alan@reeveszone.com>
2025-02-11 15:46:08 +0000

    Remove duplicates from root

--- main-code/testCallDoget.js
+++
@@ -1,7 +0,0 @@
-/* Doesn't work, returns a 404 BADTEST*/
-function testHttpGet() {
-  const scriptId = "1DZnlERwr4XjPlw03n4nlgsv5q9Vw3eYCsZB3B3IWrOZaiTleOeA8hpxA";
-  const url = "https://script.google.com/macros/s/" + scriptId + "/exec"; 
-  const response = UrlFetchApp.fetch(url); 
-  Logger.log(response.getContentText()); 
-}
